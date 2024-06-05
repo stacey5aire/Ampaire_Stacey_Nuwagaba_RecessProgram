299f511275fa011128d51fc81570a75aad9ab1a3
stacey5aire <staceyampaire3@gmail.com>
2024-06-05 20:17:55 +0300

    assignment

---
+++ Recess Assignment 1/Comments.py
@@ -0,0 +1,17 @@
+x=10
+y = 15
+#The above are variables and this is a comment
+
+#To add a multiline comment you could insert a # for each line:
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+#You can use a multiline string
+"""This is a multi line comment.
+Python will ignore string
+literals that are not assigned 
+to a variable"""
---
+++ Recess Assignment 1/Syntax.py
@@ -0,0 +1,39 @@
+#1. Syntax
+#Executing python syntax
+print("Hello World!")
+
+#Python Indentation
+if 10>5:
+    print("10 is greater than 5")
+else:
+    print("5 is greater than 10")    
+
+#Python Variables
+a=10    #integer
+b="Stacey"      #String
+c=True     #Boolean
+d=10.5     #Float
+colors = ["blue", "yellow", "green"]   #list
+
+# Print variables
+print("a =", a)
+print("b =", b)
+print("d =", d)
+print("c =", c)
+print("colors =", colors)
+
+# Conditional Statements
+if 10>5:
+    print("10 is greater than 5")
+else:
+    print("5 is greater than 10")   
+
+#functions
+def my_function():
+  print("Hello i am a function")
+
+my_function()
+
+
+
+
